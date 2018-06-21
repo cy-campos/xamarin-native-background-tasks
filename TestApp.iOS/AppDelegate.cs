1e56a4322742723608b2a20c8abcbd1fb54211fb
cy-campos <cy.campos1983@gmail.com>
2018-06-21 08:15:45 -0400

    Added project items

---
+++ TestApp.iOS/AppDelegate.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using ObjCRuntime;
+using UIKit;
+
+namespace TestApp.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        //
+        // This method is invoked when the application has loaded and is ready to run. In this 
+        // method you should instantiate the window, load the UI into it and then make the window
+        // visible.
+        //
+        // You have 17 seconds to return from this method, or iOS will terminate your application.
+        //
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            Console.WriteLine("AppDelegate : FinishedLaunching");
+
+            global::Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+
+            var blah = new SimpleBackgroundTransferViewController();
+            blah.Start();
+
+            return base.FinishedLaunching(app, options);
+        }
+
+        public Action BackgroundSessionCompletionHandler { get; set; }
+
+        public override void HandleEventsForBackgroundUrl(UIApplication application, string sessionIdentifier, Action completionHandler)
+        {
+            Console.WriteLine("HandleEventsForBackgroundUrl");
+            BackgroundSessionCompletionHandler = completionHandler;
+        }
+
+        public override void OnActivated(UIApplication uiApplication)
+        {
+            base.OnActivated(uiApplication);
+            Console.WriteLine("AppDelegate : OnActivated");
+        }
+
+        public override void OnResignActivation(UIApplication uiApplication)
+        {
+            base.OnResignActivation(uiApplication);
+            Console.WriteLine("AppDelegate : OnResignActivation");
+        }
+
+        public override void DidEnterBackground(UIApplication uiApplication)
+        {
+            base.DidEnterBackground(uiApplication);
+            Console.WriteLine("AppDelegate : DidEnterBackground");
+        }
+
+        public override void WillEnterForeground(UIApplication uiApplication)
+        {
+            base.WillEnterForeground(uiApplication);
+            Console.WriteLine("AppDelegate : WillEnterForeground");
+        }
+
+        public override void WillTerminate(UIApplication uiApplication)
+        {
+            base.WillTerminate(uiApplication);
+            Console.WriteLine("AppDelegate : WillTerminate");
+        }
+    }
+}
---
+++ TestApp.iOS/SimpleBackgroundTransferViewController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+
+using Foundation;
+using TestApp.iOS;
+using UIKit;
+
+namespace TestApp
+{
+
+    public class SimpleBackgroundTransferViewController
+    {
+
+        const string Identifier = "com.SimpleBackgroundTransfer.BackgroundSession";
+        //const string DownloadUrlString = "https://upload.wikimedia.org/wikipedia/commons/9/97/The_Earth_seen_from_Apollo_17.jpg";
+        const string DownloadUrlString = "https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_640x360.m4v";
+
+        public NSUrlSessionDownloadTask downloadTask;
+        public NSUrlSession session;
+
+        public SimpleBackgroundTransferViewController()
+        {
+            if (session == null)
+                session = InitBackgroundSession();
+        }
+
+        public void Start()
+        {
+            if (downloadTask != null)
+                return;
+
+            using (var url = NSUrl.FromString(DownloadUrlString))
+            using (var request = NSUrlRequest.FromUrl(url))
+            {
+                downloadTask = session.CreateDownloadTask(request);
+                downloadTask.Resume();
+            }
+        }
+
+        public NSUrlSession InitBackgroundSession()
+        {
+            Console.WriteLine("InitBackgroundSession");
+            using (var configuration = NSUrlSessionConfiguration.CreateBackgroundSessionConfiguration(Identifier))
+            {
+                return NSUrlSession.FromConfiguration(configuration, (INSUrlSessionDelegate)new UrlSessionDelegate(this), null);
+            }
+        }
+
+
+
+        public class UrlSessionDelegate : NSUrlSessionDownloadDelegate, INSUrlSessionDelegate
+        {
+            public SimpleBackgroundTransferViewController controller;
+
+            public UrlSessionDelegate(SimpleBackgroundTransferViewController controller)
+            {
+                this.controller = controller;
+            }
+
+            public override void DidWriteData(NSUrlSession session, NSUrlSessionDownloadTask downloadTask, long bytesWritten, long totalBytesWritten, long totalBytesExpectedToWrite)
+            {
+                Console.WriteLine("Set Progress");
+                if (downloadTask == controller.downloadTask)
+                {
+                    float progress = totalBytesWritten / (float)totalBytesExpectedToWrite;
+                    Console.WriteLine(string.Format("DownloadTask: {0}  progress: {1}", downloadTask, progress));
+
+
+
+                    InvokeOnMainThread(async () =>
+                    {
+                        await System.Threading.Tasks.Task.Delay(100000);
+                    });
+                }
+            }
+
+            public override void DidFinishDownloading(NSUrlSession session, NSUrlSessionDownloadTask downloadTask, NSUrl location)
+            {
+                Console.WriteLine("Finished");
+                Console.WriteLine("File downloaded in : {0}", location);
+                NSFileManager fileManager = NSFileManager.DefaultManager;
+
+                var URLs = fileManager.GetUrls(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomain.User);
+                NSUrl documentsDictionry = URLs[0];
+
+                NSUrl originalURL = downloadTask.OriginalRequest.Url;
+                NSUrl destinationURL = documentsDictionry.Append("image1.png", false);
+                NSError removeCopy;
+                NSError errorCopy;
+
+                fileManager.Remove(destinationURL, out removeCopy);
+                bool success = fileManager.Copy(location, destinationURL, out errorCopy);
+
+                if (success)
+                {
+                    // we do not need to be on the main/UI thread to load the UIImage
+                    UIImage image = UIImage.FromFile(destinationURL.Path);
+                    InvokeOnMainThread(() =>
+                    {
+                        
+                    });
+                }
+                else
+                {
+                    Console.WriteLine("Error during the copy: {0}", errorCopy.LocalizedDescription);
+                }
+            }
+
+            public override void DidCompleteWithError(NSUrlSession session, NSUrlSessionTask task, NSError error)
+            {
+                Console.WriteLine("DidComplete");
+                if (error == null)
+                    Console.WriteLine("Task: {0} completed successfully", task);
+                else
+                    Console.WriteLine("Task: {0} completed with error: {1}", task, error.LocalizedDescription);
+
+                float progress = task.BytesReceived / (float)task.BytesExpectedToReceive;
+                InvokeOnMainThread(() =>
+                {
+                    
+                });
+
+                controller.downloadTask = null;
+            }
+
+            public override void DidResume(NSUrlSession session, NSUrlSessionDownloadTask downloadTask, long resumeFileOffset, long expectedTotalBytes)
+            {
+                Console.WriteLine("DidResume");
+            }
+
+            public override void DidFinishEventsForBackgroundSession(NSUrlSession session)
+            {
+                using (AppDelegate appDelegate = UIApplication.SharedApplication.Delegate as AppDelegate)
+                {
+                    var handler = appDelegate.BackgroundSessionCompletionHandler;
+                    if (handler != null)
+                    {
+                        appDelegate.BackgroundSessionCompletionHandler = null;
+                        handler();
+                    }
+                }
+
+                Console.WriteLine("All tasks are finished");
+            }
+        }
+    }
+}
