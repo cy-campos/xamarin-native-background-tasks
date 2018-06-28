ad310d2bc73daf3feed987075ad661c0b5439577
cy-campos <cy.campos1983@gmail.com>
2018-06-28 11:18:24 -0400

    Implemented IDownloader to iOS

--- TestApp.iOS/AppDelegate.cs
+++ TestApp.iOS/AppDelegate.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using System.Threading;
+using System.Threading.Tasks;
 using Foundation;
 using ObjCRuntime;
 using UIKit;
@@ -21,55 +22,105 @@ namespace TestApp.iOS
         //
         // You have 17 seconds to return from this method, or iOS will terminate your application.
         //
-        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        public override bool FinishedLaunching(UIApplication uiApplication, NSDictionary launchOptions)
         {
-            Console.WriteLine("AppDelegate : FinishedLaunching");
+            System.Diagnostics.Debug.WriteLine("AppDelegate : FinishedLaunching");
 
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
-            var blah = new Downloader();
-            blah.Start();
+            //await Task.Run(() => { testDownload(); });
+            //testDownload();
+            testDownloadCancel();
+            
+            return base.FinishedLaunching(uiApplication, launchOptions);
+        }
+        
+        private async void testDownload()
+        {
+            // onStart, download something            
+            var dms = new Downloader();
+            dms.DownloadCompletedEventHandler += ((object sender, EventArgs e) =>
+            {
+                System.Diagnostics.Debug.WriteLine("DownloadCompleted Event Fired.");
+            });
 
-            return base.FinishedLaunching(app, options);
+            await dms.GetFileFromUrl("http://mirror.cessen.com/blender.org/peach/trailer/trailer_iphone.m4v");
+            //await dms.GetFileFromUrl("hasdfasdfa");
+            //await dms.GetFileFromUrl("http://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_480p_surround-fix.avi");
+        }
+        
+        private void testDownloadCancel() 
+        {
+            var dms = new Downloader();
+            
+            // test cancel token
+            var cts = new CancellationTokenSource();
+            var cancelToken = cts.Token;
+
+            int lastVal = 0;
+            var onPercentUpdate = new Action<int>((percentAsInteger) => 
+            {
+                if (percentAsInteger != lastVal)
+                {
+                    System.Diagnostics.Debug.WriteLine(String.Format("Progress: {0}%", percentAsInteger));
+                    lastVal = percentAsInteger;
+                }
+            });
+            
+            var po = new ParallelOptions();
+
+            var a1 = new Action(async () => 
+            { 
+                System.Diagnostics.Debug.WriteLine("Running download");
+                await dms.GetFileFromUrl("http://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_480p_surround-fix.avi", onPercentUpdate, cancelToken);
+            });
+
+            var a2 = new Action(() =>
+            {
+                System.Diagnostics.Debug.WriteLine("10 secs till cancel");
+                cts.CancelAfter(10000);
+            });
+
+            Parallel.Invoke(po, a1, a2);
         }
 
         public Action BackgroundSessionCompletionHandler { get; set; }
 
         public override void HandleEventsForBackgroundUrl(UIApplication application, string sessionIdentifier, Action completionHandler)
         {
-            Console.WriteLine("HandleEventsForBackgroundUrl");
+            System.Diagnostics.Debug.WriteLine("HandleEventsForBackgroundUrl");
             BackgroundSessionCompletionHandler = completionHandler;
         }
 
         public override void OnActivated(UIApplication uiApplication)
         {
             base.OnActivated(uiApplication);
-            Console.WriteLine("AppDelegate : OnActivated");
+            System.Diagnostics.Debug.WriteLine("AppDelegate : OnActivated");
         }
 
         public override void OnResignActivation(UIApplication uiApplication)
         {
             base.OnResignActivation(uiApplication);
-            Console.WriteLine("AppDelegate : OnResignActivation");
+            System.Diagnostics.Debug.WriteLine("AppDelegate : OnResignActivation");
         }
 
         public override void DidEnterBackground(UIApplication uiApplication)
         {
             base.DidEnterBackground(uiApplication);
-            Console.WriteLine("AppDelegate : DidEnterBackground");
+            System.Diagnostics.Debug.WriteLine("AppDelegate : DidEnterBackground");
         }
 
         public override void WillEnterForeground(UIApplication uiApplication)
         {
             base.WillEnterForeground(uiApplication);
-            Console.WriteLine("AppDelegate : WillEnterForeground");
+            System.Diagnostics.Debug.WriteLine("AppDelegate : WillEnterForeground");
         }
 
         public override void WillTerminate(UIApplication uiApplication)
         {
             base.WillTerminate(uiApplication);
-            Console.WriteLine("AppDelegate : WillTerminate");
+            System.Diagnostics.Debug.WriteLine("AppDelegate : WillTerminate");
         }
     }
 }
--- TestApp.iOS/Downloader.cs
+++ TestApp.iOS/Downloader.cs
@@ -1,146 +1,177 @@
 ﻿using System;
-
+using System.Threading;
+using System.Threading.Tasks;
 using Foundation;
 using TestApp.iOS;
 using UIKit;
 
 namespace TestApp
 {
-
-    public class SimpleBackgroundTransferViewController
+    public class Downloader : IDownloader
     {
+        public long TotalSizeBytes { get; set; }
+        public long TotalBytesDownloaded { get; set; }
 
-        const string Identifier = "com.SimpleBackgroundTransfer.BackgroundSession";
-        //const string DownloadUrlString = "https://upload.wikimedia.org/wikipedia/commons/9/97/The_Earth_seen_from_Apollo_17.jpg";
-        const string DownloadUrlString = "https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_640x360.m4v";
+        public int PercentageDownloaded { get { throw new NotImplementedException(); } }
+
+        public event EventHandler DownloadCompletedEventHandler;
+        private void onDownloadCompleted(object sender, EventArgs e) { DownloadCompletedEventHandler?.Invoke(sender, e); }
+
+        public event EventHandler DownloadFailedEventHandler;
+        private void onDownloadFailed(object sender, EventArgs eventArgs) { DownloadFailedEventHandler?.Invoke(sender, eventArgs); }
+
+        public async Task GetFileFromUrl(string url)
+        {
+            await GetFileFromUrl(url, null, null);
+        }
 
         public NSUrlSessionDownloadTask downloadTask;
         public NSUrlSession session;
-
-        public SimpleBackgroundTransferViewController()
+        const string identifier = "com.SimpleBackgroundTransfer.BackgroundSession";
+        
+        public async Task GetFileFromUrl(string url, Action<int> onPercentUpdate, object CancellationToken)
         {
-            if (session == null)
-                session = InitBackgroundSession();
+            await Task.Run(() =>
+            {
+                // start a background session if not started already    
+                session = session ?? initBackgroundSession(onPercentUpdate, CancellationToken);
+
+                // get a NSUrl, get a request, and queue the download
+                using (var nsurl = NSUrl.FromString(url))
+                using (var request = NSUrlRequest.FromUrl(nsurl))
+                {
+                    downloadTask = session.CreateDownloadTask(request);
+                    downloadTask.Resume();
+                }
+            });   
         }
 
-        public void Start()
+        private UrlSessionDelegate _urlSessionDelegate;
+        private NSUrlSession initBackgroundSession(Action<int> onPercentUpdate, object CancellationToken)
         {
-            if (downloadTask != null)
-                return;
-
-            using (var url = NSUrl.FromString(DownloadUrlString))
-            using (var request = NSUrlRequest.FromUrl(url))
+            // Because eventually NSUrlSession.FromConfiguration must receive an interface as a param (INSUrlSessionDelegate),
+            // we have to assign whatever properties we are listening for here.
+            _urlSessionDelegate = new UrlSessionDelegate(this, onPercentUpdate, CancellationToken);
+            
+            System.Diagnostics.Debug.WriteLine("InitBackgroundSession");
+            using (var configuration = NSUrlSessionConfiguration.CreateBackgroundSessionConfiguration(identifier))
             {
-                downloadTask = session.CreateDownloadTask(request);
-                downloadTask.Resume();
+                return NSUrlSession.FromConfiguration(configuration, (INSUrlSessionDelegate)_urlSessionDelegate, null);
             }
         }
+    }
+    
+    public class UrlSessionDelegate : NSUrlSessionDownloadDelegate, INSUrlSessionDelegate
+    {
+        private Downloader controller;
 
-        public NSUrlSession InitBackgroundSession()
+        private Action<int> onPercentUpdate;
+        private CancellationToken CancellationToken;
+        
+        public UrlSessionDelegate(Downloader controller, Action<int> onPercentUpdate, object CancellationToken)
         {
-            Console.WriteLine("InitBackgroundSession");
-            using (var configuration = NSUrlSessionConfiguration.CreateBackgroundSessionConfiguration(Identifier))
-            {
-                return NSUrlSession.FromConfiguration(configuration, (INSUrlSessionDelegate)new UrlSessionDelegate(this), null);
-            }
+            this.controller = controller;
+            //this.onPercentUpdate = onPercentUpdate;
+            //this.CancellationToken = CancellationToken;
+            
+            // set up onPercentUpdate - Action should take an integer parameter and display it somehow
+            this.onPercentUpdate = onPercentUpdate ?? ((percentageAsInteger) => { });
+
+            // handle object CancellationToken, since this can be null
+            this.CancellationToken = CancellationToken != null ? (CancellationToken)CancellationToken : new CancellationToken();
+
+            // check to see if cancelled first
+            if (this.CancellationToken.IsCancellationRequested)
+                this.CancellationToken.ThrowIfCancellationRequested();
         }
 
-
-
-        public class UrlSessionDelegate : NSUrlSessionDownloadDelegate, INSUrlSessionDelegate
+        public override void DidWriteData(NSUrlSession session, NSUrlSessionDownloadTask downloadTask, long bytesWritten, long totalBytesWritten, long totalBytesExpectedToWrite)
         {
-            public SimpleBackgroundTransferViewController controller;
-
-            public UrlSessionDelegate(SimpleBackgroundTransferViewController controller)
+            if (downloadTask == controller.downloadTask)
             {
-                this.controller = controller;
+                //System.Diagnostics.Debug.WriteLine(string.Format("DownloadTask: {0}  progress: {1}/{2}", downloadTask, totalBytesWritten, totalBytesExpectedToWrite));
+
+                onPercentUpdate(_getPercentageDownloaded(totalBytesWritten, totalBytesExpectedToWrite));
+                CancellationToken.ThrowIfCancellationRequested();
+                //InvokeOnMainThread(async () =>
+                //{
+                //    await System.Threading.Tasks.Task.Delay(100000);
+                //});
             }
+        }
+        
+        private int _getPercentageDownloaded(long totalBytesWritten, long totalBytesExpectedToWrite)
+        {
+            return totalBytesExpectedToWrite < 1 ? 0 : (int)(((double)totalBytesWritten / (double)totalBytesExpectedToWrite) * 100);
+        }
 
-            public override void DidWriteData(NSUrlSession session, NSUrlSessionDownloadTask downloadTask, long bytesWritten, long totalBytesWritten, long totalBytesExpectedToWrite)
+        public override void DidFinishDownloading(NSUrlSession session, NSUrlSessionDownloadTask downloadTask, NSUrl location)
+        {
+            System.Diagnostics.Debug.WriteLine("Finished");
+            System.Diagnostics.Debug.WriteLine("File downloaded in : {0}", location);
+            //NSFileManager fileManager = NSFileManager.DefaultManager;
+
+            //var URLs = fileManager.GetUrls(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomain.User);
+            //NSUrl documentsDictionry = URLs[0];
+
+            //NSUrl originalURL = downloadTask.OriginalRequest.Url;
+            //NSUrl destinationURL = documentsDictionry.Append("image1.png", false);
+            //NSError removeCopy;
+            //NSError errorCopy;
+
+            //fileManager.Remove(destinationURL, out removeCopy);
+            //bool success = fileManager.Copy(location, destinationURL, out errorCopy);
+
+            //if (success)
+            //{
+            //    // we do not need to be on the main/UI thread to load the UIImage
+            //    UIImage image = UIImage.FromFile(destinationURL.Path);
+            //    InvokeOnMainThread(() =>
+            //    {
+
+            //    });
+            //}
+            //else
+            //{
+            //    System.Diagnostics.Debug.WriteLine("Error during the copy: {0}", errorCopy.LocalizedDescription);
+            //}
+        }
+
+        public override void DidCompleteWithError(NSUrlSession session, NSUrlSessionTask task, NSError error)
+        {
+            System.Diagnostics.Debug.WriteLine("DidComplete");
+            if (error == null)
+                System.Diagnostics.Debug.WriteLine("Task: {0} completed successfully", task);
+            else
+                System.Diagnostics.Debug.WriteLine("Task: {0} completed with error: {1}", task, error.LocalizedDescription);
+
+            float progress = task.BytesReceived / (float)task.BytesExpectedToReceive;
+            InvokeOnMainThread(() =>
             {
-                Console.WriteLine("Set Progress");
-                if (downloadTask == controller.downloadTask)
+
+            });
+
+            controller.downloadTask = null;
+        }
+
+        public override void DidResume(NSUrlSession session, NSUrlSessionDownloadTask downloadTask, long resumeFileOffset, long expectedTotalBytes)
+        {
+            System.Diagnostics.Debug.WriteLine("DidResume");
+        }
+
+        public override void DidFinishEventsForBackgroundSession(NSUrlSession session)
+        {
+            using (AppDelegate appDelegate = UIApplication.SharedApplication.Delegate as AppDelegate)
+            {
+                var handler = appDelegate.BackgroundSessionCompletionHandler;
+                if (handler != null)
                 {
-                    float progress = totalBytesWritten / (float)totalBytesExpectedToWrite;
-                    Console.WriteLine(string.Format("DownloadTask: {0}  progress: {1}", downloadTask, progress));
-
-
-
-                    InvokeOnMainThread(async () =>
-                    {
-                        await System.Threading.Tasks.Task.Delay(100000);
-                    });
+                    appDelegate.BackgroundSessionCompletionHandler = null;
+                    handler();
                 }
             }
 
-            public override void DidFinishDownloading(NSUrlSession session, NSUrlSessionDownloadTask downloadTask, NSUrl location)
-            {
-                Console.WriteLine("Finished");
-                Console.WriteLine("File downloaded in : {0}", location);
-                NSFileManager fileManager = NSFileManager.DefaultManager;
-
-                var URLs = fileManager.GetUrls(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomain.User);
-                NSUrl documentsDictionry = URLs[0];
-
-                NSUrl originalURL = downloadTask.OriginalRequest.Url;
-                NSUrl destinationURL = documentsDictionry.Append("image1.png", false);
-                NSError removeCopy;
-                NSError errorCopy;
-
-                fileManager.Remove(destinationURL, out removeCopy);
-                bool success = fileManager.Copy(location, destinationURL, out errorCopy);
-
-                if (success)
-                {
-                    // we do not need to be on the main/UI thread to load the UIImage
-                    UIImage image = UIImage.FromFile(destinationURL.Path);
-                    InvokeOnMainThread(() =>
-                    {
-                        
-                    });
-                }
-                else
-                {
-                    Console.WriteLine("Error during the copy: {0}", errorCopy.LocalizedDescription);
-                }
-            }
-
-            public override void DidCompleteWithError(NSUrlSession session, NSUrlSessionTask task, NSError error)
-            {
-                Console.WriteLine("DidComplete");
-                if (error == null)
-                    Console.WriteLine("Task: {0} completed successfully", task);
-                else
-                    Console.WriteLine("Task: {0} completed with error: {1}", task, error.LocalizedDescription);
-
-                float progress = task.BytesReceived / (float)task.BytesExpectedToReceive;
-                InvokeOnMainThread(() =>
-                {
-                    
-                });
-
-                controller.downloadTask = null;
-            }
-
-            public override void DidResume(NSUrlSession session, NSUrlSessionDownloadTask downloadTask, long resumeFileOffset, long expectedTotalBytes)
-            {
-                Console.WriteLine("DidResume");
-            }
-
-            public override void DidFinishEventsForBackgroundSession(NSUrlSession session)
-            {
-                using (AppDelegate appDelegate = UIApplication.SharedApplication.Delegate as AppDelegate)
-                {
-                    var handler = appDelegate.BackgroundSessionCompletionHandler;
-                    if (handler != null)
-                    {
-                        appDelegate.BackgroundSessionCompletionHandler = null;
-                        handler();
-                    }
-                }
-
-                Console.WriteLine("All tasks are finished");
-            }
+            System.Diagnostics.Debug.WriteLine("All tasks are finished");
         }
     }
 }
